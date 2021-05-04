baecdef2b388ef094b774f6123449da32ca74a47
Jun Nguyen <n_jun01@hotmail.com>
2021-05-04 22:35:46 +0900

    initial commit

---
+++ Main.py
@@ -0,0 +1,49 @@
+gameBoard = [[None, None, None], [None, None, None], [None, None, None]]
+
+# Basic flow
+# 1. print title of this game
+# 2. Start Loop
+# 3. print gameBoard
+# 4. get input from the player who is playing "X"
+# 5. Check if X wins,
+# 6. print he gameBoard
+# 7. Finish the game if the player wins
+# 8. get input from the player who is playing "O"
+# 9. Check if O wins,
+# 10. print he gameBoard
+# 11. Finish the game if the player wins
+# 12. end of loop
+
+# TODO implement main logic here
+
+
+
+# print the content of the board like below
+#    |   |
+# -----------
+#    |   |
+# -----------
+#    |   |
+
+def print_board(gameBoard):
+    # TODO implement logic here
+    return
+
+
+# Don't return anything if there is no winner, return O or X if there if either side won the game
+def get_winner():
+    # TODO implement logic here
+    return
+
+
+# Get side as argument, this should be "x" or "o".
+# Get index as argument, then update the cell on the board accordingly
+# Index of the board
+#  7 | 8 | 9
+# -----------
+#  4 | 5 | 6
+# -----------
+#  1 | 2 | 3
+def write_to_board(side, index):
+    # TODO implement logic here
+    return
